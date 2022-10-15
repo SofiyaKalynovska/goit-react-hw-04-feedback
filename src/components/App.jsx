fa88da598ace5ac54133565586afe3e9c7892943
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2022-10-15 17:24:08 +0300

    Create simple feedback counter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import {
+  Wrapper,
+  SectionTitle,
+  OptionsSection,
+  FeedbackOption,
+} from './App.styled';
+
+export default class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+  handleClick(option) {
+    this.setState(prevState => ({
+      [option]: prevState[option] + 1
+    }));
+  };
+
+  render() {
+    const  option  = this.state;
+    return (
+      <Wrapper>
+        <SectionTitle>Please leave feedback</SectionTitle>
+        <OptionsSection>
+          <FeedbackOption type="button" onClick={() => this.handleClick('good')}>Good</FeedbackOption>
+          <FeedbackOption type="button" onClick={() => this.handleClick('neutral')}>Neutral</FeedbackOption>
+          <FeedbackOption type="button" onClick={() => this.handleClick('bad')}>Bad</FeedbackOption>
+        </OptionsSection>
+
+        <SectionTitle>Statistics</SectionTitle>
+        <p>Good: {option.good }</p>
+        <p>Neutral: {option.neutral}</p>
+        <p>Bad: {option.bad}</p>
+      </Wrapper>
+    );
+  }
+  
+}
+  
+
---
+++ src/components/App.styled.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: '100vh';
+  display: 'flex';
+  justify-content: 'center';
+  align-items: 'center';
+  font-size: 40;
+  color: '#010101';
+  padding: 30px;
+`
+
+export const SectionTitle = styled.h2`
+color: black;
+font-size: 32;
+`
+
+export const OptionsSection = styled.div`
+width: 400px;
+display: flex;
+justify-content: space-between;
+
+`
+
+export const FeedbackOption = styled.button`
+width: 100px;
+height: 20px;
+`
